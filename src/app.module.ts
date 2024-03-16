4f1ada675df345ca9c40c21c87af29f0d9230cd0
THT909 <hiepthanha7@gmail.com>
2024-03-16 08:48:09 +0700

    config:swagger, .env, mongoose/mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_LOCALHOST, {
+      connectionFactory: (connection) => {
+        connection.on('connected', () => {
+          console.log('\x1b[36m%s\x1b[0m', 'DATABASE IS CONNECTED !!');
+        });
+        connection._events.connected();
+        return connection;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  //config swagger
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  //config PORT
+  const PORT = process.env.PORT || 8080;
+  await app.listen(PORT);
+  console.log(`Sever is running at ${await app.getUrl()}`);
 }
 bootstrap();
