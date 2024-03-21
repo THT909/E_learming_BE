a001eb60454663833c8d580a9503cf48ed69a688
THT909 <hiepthanha7@gmail.com>
2024-03-21 14:38:03 +0700

    feat: access_token and refresh_token

--- src/common/auth/auth.controller.ts
+++ src/common/auth/auth.controller.ts
@@ -1,24 +1,46 @@
 import {
   Body,
   Controller,
+  Get,
   HttpException,
+  Param,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { AuthPayLoadDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
 
 import { AuthGuard } from '@nestjs/passport';
+import { ApiResponse, ApiBody } from '@nestjs/swagger';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
   login(@Body() authPayLoad: AuthPayLoadDto) {
+    // if(!authPayLoad.email){
+    //   throw new
+    // }
+
     const user = this.authService.validateUser(authPayLoad);
     if (!user) {
       throw new HttpException('invalid', 401);
     }
     return user;
+  }
+  @Post('refresh')
+  @ApiBody({
+    schema: { example: { refresh_token: 'your_refresh_token_here' } },
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Refreshed access token successfully.',
+  })
+  async getAccessToken(@Body() body: { refresh_token: string }) {
+    const { refresh_token } = body;
+    const data = await this.authService.getAccessToken(refresh_token);
+
+    return data;
   }
 }
--- src/common/auth/auth.service.ts
+++ src/common/auth/auth.service.ts
@@ -16,23 +16,29 @@ export class AuthService {
     const user: UserDocument = await this.userService.findByEmail(email);
 
     const validatePassword = await bcrypt.compare(password, user.password);
-    // console.log(validatePassword);
-    // console.log(password);
-    // console.log(user.password);
     if (!user) {
       return null;
     }
     if (validatePassword) {
-      const { _id, username, role } = user.toObject();
-      const data = {
-        header: this.jwtService.sign({ _id, username }),
-        body: {
-          username,
-          role,
-        },
-      };
-
-      return data;
+      const { _id, username } = user.toObject();
+      const access_token = this.jwtService.sign({ _id, username });
+      const refresh_token = this.jwtService.sign({ _id }, { expiresIn: '7d' });
+      return { access_token, refresh_token };
     }
+  }
+  async getAccessToken(token: string): Promise<any> {
+    try {
+      const data = await this.decodeToken(token);
+      const user: UserDocument = await this.userService.findOne(data._id);
+      const { _id, username } = user.toObject();
+      const access_token = this.jwtService.sign({ _id, username });
+      return { access_token };
+    } catch (error) {
+      console.error('Error decoding token:', error.message);
+      return null;
+    }
+  }
+  decodeToken(token: string): Promise<any> {
+    return this.jwtService.verify(token);
   }
 }
